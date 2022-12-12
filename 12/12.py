a37667199498cd8f27b2d8bc704066ff464f88f5
perugi <dominik.perusko@gmail.com>
2022-12-12 23:17:36 +0100

    Add solution for part two, change search algo to bfs

--- 12/12.py
+++ 12/12.py
@@ -1,6 +1,7 @@
 from collections import namedtuple
 from pprint import pprint
 import math
+import copy
 
 filename = "input.txt"
 
@@ -14,7 +15,7 @@ with open(filename) as f:
             elif char == "E":
                 char = "z"
                 end = (col, row)
-            nodes[(col, row)] = {"height": char, "paths": [], "distance": math.inf}
+            nodes[(col, row)] = {"height": char, "paths": [], "parent": None}
 
 dimension_x = col
 dimension_y = row
@@ -38,22 +39,57 @@ for node in nodes:
         if elevation_diff <= 1:
             nodes[node]["paths"].append(dest_node)
 
-# Let's get some Dijkstra action up in here
-def get_min_path(nodes, start, end):
-    nodes[start]["distance"] = 0
-    distances = {}
-    while nodes:
-        active_node = min(nodes, key=lambda x: nodes[x]["distance"])
-        current_dis = nodes[active_node]["distance"]
-        for dest_node in nodes[active_node]["paths"]:
-            if dest_node in distances:
-                continue
-            elif current_dis + 1 < nodes[dest_node]["distance"]:
-                nodes[dest_node]["distance"] = current_dis + 1
-        distances[active_node] = nodes.pop(active_node)
+# A first attempt with Dijkstra, does not play nice with Part Two
+# def get_min_path(nodes, start, end):
+#     nodes[start]["distance"] = 0
+#     distances = {}
+#     while nodes:
+#         active_node = min(nodes, key=lambda x: nodes[x]["distance"])
+#         current_dis = nodes[active_node]["distance"]
+#         for dest_node in nodes[active_node]["paths"]:
+#             if dest_node in distances:
+#                 continue
+#             elif current_dis + 1 < nodes[dest_node]["distance"]:
+#                 nodes[dest_node]["distance"] = current_dis + 1
+#         distances[active_node] = nodes.pop(active_node)
 
-    return distances[end]["distance"]
+#     return distances[end]["distance"]
+
+
+def bfs(nodes, start, end):
+    visited, queue = [], [start]
+
+    while True:
+        active_node = queue.pop(0)
+        if active_node == end:
+            break
+        for destination in nodes[active_node]["paths"]:
+            if destination not in visited and destination not in queue:
+                nodes[destination]["parent"] = active_node
+                queue.append(destination)
+        visited.append(active_node)
+
+        if not queue:
+            return None
+
+    distance = 0
+    while nodes[active_node]["parent"] != None:
+        distance += 1
+        active_node = nodes[active_node]["parent"]
+
+    return distance
 
 
 """ Part One """
-print(f"Distance to E: {get_min_path(nodes, start, end)}")
+print(f"Distance to E (Part One): {bfs(copy.deepcopy(nodes), start, end)}")
+
+""" Part Two """
+start_positions = [node for node in nodes if nodes[node]["height"] == "a"]
+distances = {}
+for i, start in enumerate(start_positions):
+    distance = bfs(copy.deepcopy(nodes), start, end)
+    if distance:
+        distances[start] = distance
+    print(f"{i+1}/{len(start_positions)} {start}: {distance}")
+
+print(f"Min distance to E (Part Two): {min(distances.values())}")
